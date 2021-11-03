5afa50fe4adda070f6a7488274850f3c6578e8b1
Julian Tran <juliantran003@gmail.com>
2021-11-03 14:07:24 +0000

    added photo gallery

--- src/containers/Gallery.js
+++ src/containers/Gallery.js
@@ -1,5 +1,63 @@
+// Packages
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+// Compoenents
+import Loader from "../components/Utility/Loader";
+
 const Gallery = () => {
-  return <div></div>;
+  // States
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [modal, setModal] = useState(false);
+  const [modalInfo, setModalInfo] = useState();
+  // Fetch pictures
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://swaaramusic-backend.herokuapp.com/pictures`
+        );
+        setData(response.data.resources);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
+  // Modal Handle
+  const modalHandle = (props) => {
+    console.log(props.info);
+    setModalInfo(props.info);
+    setModal(true);
+  };
+  return isLoading ? (
+    <Loader />
+  ) : (
+    <div className="gallery">
+      {
+        <div className="gallery__container">
+          {modal ? (
+            <div className="gallery__modal">
+              <i class="fas fa-times" onClick={() => setModal(false)}></i>
+              <img src={modalInfo} alt={modalInfo} />
+            </div>
+          ) : (
+            data.map((picture, index) => {
+              return (
+                <img
+                  src={picture.url}
+                  alt={picture.url}
+                  onClick={() => modalHandle({ info: picture.url })}
+                />
+              );
+            })
+          )}
+        </div>
+      }
+    </div>
+  );
 };
 
 export default Gallery;
--- src/containers/Videos.js
+++ src/containers/Videos.js
@@ -1,4 +1,23 @@
+// Packages
+import axios from "axios";
+import { useState, useEffect } from "react";
+require("dotenv").config();
+
 const Videos = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=UUUUs-ri1zJU_mYumBbzjO6g&key=AIzaSyCKsqzC5bcbGo5LwAXJJbvEW5qRUSVc-P8`
+        );
+
+        console.log(response.data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
   return <div></div>;
 };
 
