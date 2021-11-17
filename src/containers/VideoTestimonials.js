18894764afc456ba72c8f045afaa81c98a3ca5df
Julian Tran <juliantran003@gmail.com>
2021-11-17 14:10:44 +0000

    footer adjustments on certain pages

--- src/containers/VideoTestimonials.js
+++ src/containers/VideoTestimonials.js
@@ -1,9 +1,13 @@
+// Components
+import Footer from "../components/Footer";
+
 const VideoTestimonials = () => {
   return (
     <div className="videoTestimonials">
       <div className="videoTestimonials__container">
         <h1 className="txt-header-white">COMING SOON</h1>
       </div>
+      <Footer />
     </div>
   );
 };
