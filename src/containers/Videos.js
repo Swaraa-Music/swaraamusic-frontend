e18abee7a1c1f6c863fd92bf48f04148bcb9e030
Julian Tran <juliantran003@gmail.com>
2021-12-01 10:58:03 +0000

    hid showreel video

--- src/containers/Videos.js
+++ src/containers/Videos.js
@@ -31,11 +31,11 @@ const Videos = () => {
   ) : (
     <div className="videos bg-pink-purple-gradient">
       <div className="videos__showreel__container">
-        <h1 className="txt-header-white">Showreel</h1>
-        <video
-          controls
-          src="https://res.cloudinary.com/itrsq/video/upload/v1637939362/swaraamusic/Videos/VID-2021showrel_4_website_jd238q.mp4"
-        ></video>
+        {/* <h1 className="txt-header-white">Showreel</h1>
+          <video
+            controls
+            src="https://res.cloudinary.com/itrsq/video/upload/v1637939362/swaraamusic/Videos/VID-2021showrel_4_website_jd238q.mp4"
+          ></video> */}
         <h1 className="txt-header-white">Youtube Videos</h1>
       </div>
 
